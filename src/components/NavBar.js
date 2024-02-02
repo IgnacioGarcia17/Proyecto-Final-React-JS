b517617f450d516ff38dcd172a2aae76a1f2f530
Ignacio Garcia Madruli <ignaciomarcosgarcia@gmail.com>
2024-02-01 21:31:07 -0300

    primer pre-entrega

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './NavBar.css';
+
+const NavBar = () => {
+    return (
+        <nav className="navbar">
+            <ul>
+                <li>Inicio</li>
+                <li>Productos</li>
+                <li>Contacto</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default NavBar;
---
+++ src/components/OtroComponente.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './OtroComponente.css';
+
+const OtroComponente = () => {
+    return (
+        <div className="otro-componente">
+            <h2>Nombre de la marca</h2>
+            <img src="/images/imagen_x.jpg" alt="Logo" />
+            <p>Algun eslogan</p>
+        </div>
+    );
+}
+
+export default OtroComponente;
